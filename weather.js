0e97a6e66971bc0459336792adf1ec536e29e6e8
anturchin <tur4intwo@gmail.com>
2023-12-24 13:38:10 +0300

    feat: add args-js and type module ES6

---
+++ weather.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+import { getArgs } from './helpers/args.js';
+
+
+const initCli = () => {
+
+	const args = getArgs(process.argv)
+	console.log(args);
+
+	if (args.h) {
+
+	}
+
+	if (args.s) {
+
+	}
+
+	if (args.t) {
+
+	}
+
+}
+
+initCli();
